a5f805edb608b6dc1b3f433c49d1cc0a3a0c55b6
AlexLig <ligkalex@gmail.com>
2019-02-04 02:42:51 +0200

    validation pipe

--- src/employee/dtos/create-employee.dto.ts
+++ src/employee/dtos/create-employee.dto.ts
@@ -1,4 +1,16 @@
+import {
+  IsString,
+  Length,
+  IsNumberString,
+  IsAlphanumeric,
+} from 'class-validator';
+
 export class CreateEmployeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsNumberString({ message: 'Εισάγετε μόνο από αριθμούς' })
+  @IsAlphanumeric({ message: 'Εισάγετε μόνο από αριθμούς' })
+  @Length(9, 9, { message: 'Ο ΑΦΜ αποτελείται από 9 αριθμούς' })
   readonly vat: string;
 }
