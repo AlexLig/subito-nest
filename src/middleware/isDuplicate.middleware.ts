069b86b01346059333cb44e3fa2318fc7e7eaa41
Kostas Liberopoulos <libekonst@gmail.com>
2019-02-10 22:42:25 +0200

    Update for readability.

--- src/middleware/isDuplicate.middleware.ts
+++ src/middleware/isDuplicate.middleware.ts
@@ -5,27 +5,28 @@ import { databaseWriteException } from 'src/shared';
 type Properties = 'vat' | 'ame';
 type Entities = 'Employee' | 'Employer';
 
-const isDuplicate = (property: Properties) => (repository: Entities) => async (
-  req: any,
-  res: any,
-  next: any,
-) => {
-  const value = req.body[property];
-  let duplicate;
+const isDuplicate = (property: Properties) => (repository: Entities) => {
+  return async (req: any, res: any, next: any) => {
+    const value = req.body[property];
+    let duplicate;
 
-  try {
-    // ? If property doesnt exist for this Entity, the repo returns the first row in the table.
-    duplicate = await getRepository(repository).findOne({
-      [property]: value,
-    });
-  } catch (e) {
-    throw databaseWriteException();
-  }
+    try {
+      // ? If property doesnt exist for this Entity, the repo returns the first row in the table.
+      duplicate = await getRepository(repository).findOne({
+        [property]: value,
+      });
+    } catch (e) {
+      throw databaseWriteException();
+    }
 
-  if (duplicate) {
-    throw new HttpException(`Middleware worked! ${property} is duplicate`, 400);
-  }
-  next();
+    if (duplicate) {
+      throw new HttpException(
+        `Middleware worked! ${property} is duplicate`,
+        400,
+      );
+    }
+    next();
+  };
 };
 
 const isDuplicateVat = isDuplicate('vat');
